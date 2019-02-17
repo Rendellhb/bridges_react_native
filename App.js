00ec453baf9f19e48e6f0e3c1534ca1f4150369b
F8578229 Rendell Hoffmann Bernardes <rendell@bb.com.br>
2019-02-17 12:12:11 -0300

    configs and parsers

--- App.js
+++ App.js
@@ -7,27 +7,83 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NativeModules
+} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { something: '' };
+  }
+
+  async printname() {
+    try {
+      const something1 = await NativeModules.OTABridge.printSomething('something');
+      this.setState(() => ({
+        something: something1
+      }));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async openSessionWithToken() {
+    try {
+      this.result = await NativeModules.OTABridge.openSessionWithToken('VCpCTVlS3P535Y7ZpGjE0YGJ91dfgqRA');
+      console.log(this.result);
+    } catch (e) {
+      console.log(`Erro do OTA ${e}`);
+    }
+  }
+
+  async createKey() {
+    this.otaKeyRequest = {
+      otaId: '1',
+      extId: '1',
+      beginDate: '01-03-2019',
+      endDate: '01-03-2019',
+      vehicleId: '1',
+      vehicleExtId: '1',
+      enableNow: true,
+      tokenAmount: '16',
+      singleShotSecurity: true,
+      security: 'token'
+    };
+
+    try {
+      const result = await NativeModules.OTABridge.createKey(JSON.stringify(this.otaKeyRequest));
+      console.log(`SUCESSO: ${result}`);
+    } catch (e) {
+      console.log(`ERRO: ${e}`);
+    }
+  }
+
   render() {
+    this.printname();
+    this.createKey();
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{this.state.something}</Text>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
